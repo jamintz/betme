320ba2d6e6118b2d409a0a0b3c0b0ba4008c86da
Jason Mintz <jasonamintz@gmail.com>
2017-04-08 15:25:20 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :games
   resources :answers
   resources :guesses
   resources :props
   resources :users
+  
+  root "games#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
